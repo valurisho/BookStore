56b277f67431bb2c6c6d056d5e7f6d8253464d9e
valurisho <153689011+valurisho@users.noreply.github.com>
2025-04-01 20:50:25 -0600

    Commit Backend

--- backend/BookStore.API/Data/Book.cs
+++ backend/BookStore.API/Data/Book.cs
@@ -8,12 +8,12 @@ public class Book
     public int BookID {get; set;}
     [Required]
     public string Title {get; set;}
-    public string Author {get; set;}
-    public string Publisher {get; set;}
-    public string ISBN {get; set;}
-    public  string Classification {get; set;}
-    public string Category {get; set;}
-    public int PageCount {get; set;}
-    public float Price {get; set;}
+    public string? Author {get; set;}
+    public string? Publisher {get; set;}
+    public string? ISBN {get; set;}
+    public  string? Classification {get; set;}
+    public string? Category {get; set;}
+    public int? PageCount {get; set;}
+    public float? Price {get; set;}
     
 }
